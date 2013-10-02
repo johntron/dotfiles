117fe82b9d73c753b0907b632e1bdf2343fce1ba
John Syrinek <john.syrinek@gmail.com>
2013-10-02 21:51:48 +0000

    Made install.sh more directory-agnostic

--- install.sh
+++ install.sh
@@ -5,27 +5,32 @@
 ############################
 
 ########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gitconfig vimrc zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+OLDDIR=~/dotfiles_old             # old dotfiles backup directory
+FILES="gitconfig vimrc zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+echo "Creating $OLDDIR for backup of any existing dotfiles in ~"
+mkdir -p $OLDDIR
 echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
+echo "Changing to the $DIR directory"
+cd $DIR
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+for FILE in $FILES; do
+    echo "Moving any existing dotfiles from ~ to $OLDDIR"
+    mv ~/.$FILE $OLDDIR
+    echo "Creating symlink to $FILE in home directory."
+    ln -s $DIR/$FILE ~/.$FILE
 done
